898b18bfa4b47c9e942e5ea992fc421fe3959867
michael henrys <michaelhenrys@michaels-MacBook-Pro-2.local>
2022-02-16 20:46:53 +1300

    Added slider to change the size of elipse on canvas

--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -1,25 +1,30 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import p5 from 'p5'
 
 export default function Canvas () {
+  const [size, setSize] = useState(100)
   const processingRef = useRef()
   const Sketch = p => {
     p.setup = () => {
-      p.createCanvas(400, 400)
+      p.createCanvas(500, 500)
     }
 
     p.draw = () => {
       p.background(220)
-      p.ellipse(p.mouseX, p.mouseY, 80, 80)
+      p.ellipse(200, 200, size, size)
     }
   }
 
   useEffect(() => {
-    // eslint-disable-next-line no-unused-vars, new-cap
+    // eslint-disable-next-line new-cap
     const myP5 = new p5(Sketch, processingRef.current)
-  }, [])
+    return () => myP5.remove()
+  })
 
   return (
-    <div ref={processingRef} />
+    <>
+      <input type="range" min="10" max="150" value={size} onChange={(event) => setSize(event.target.value)}></input>
+      <div ref={processingRef} />
+    </>
   )
 }
